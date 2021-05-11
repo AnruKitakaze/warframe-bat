caca002d9cc3be0d112e7fc43896b43b81ef8e7c
AnruKitakaze <groxattack@gmail.com>
2021-05-11 16:11:04 +0700

    feat: Database reworked

--- warframe_bat_project/bat/admin.py
+++ warframe_bat_project/bat/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Weapon, WeaponAttackStats, WeaponAttackDamageDisposition
+from .models import Weapon, WeaponAttackStats
 
 
 admin.site.register(Weapon)
 admin.site.register(WeaponAttackStats)
-admin.site.register(WeaponAttackDamageDisposition)
--- warframe_bat_project/bat/models.py
+++ warframe_bat_project/bat/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Weapon(models.Model):
-    weapon_name = models.CharField(max_length=100)
+    weapon_name = models.CharField(primary_key=True, max_length=100)
     weapon_class = models.CharField(max_length=100)
 
     def __str__(self):
@@ -11,17 +11,42 @@ class Weapon(models.Model):
 
 class WeaponAttackStats(models.Model):
     weapon_name = models.ForeignKey(Weapon, on_delete=models.CASCADE)
-    attack_type = models.CharField(max_length=50)
+    attack_type = models.IntegerField()
 
-    def __str__(self):
-        return self.weapon_name.__str__()
+    crit_chance = models.FloatField(null=False, default=0.0)
+    crit_multiplier = models.FloatField(null=False, default=0.0)
+    status_chance = models.FloatField(null=False, default=0.0)
+    multishot = models.FloatField(null=False, default=0.0)
+    
+    accuracy = models.FloatField(null=False, default=0.0)
+    fire_rate = models.FloatField(null=False, default=0.0)
+    charge_speed = models.FloatField(null=False, default=0.0)
+    magazine_size = models.FloatField(null=False, default=0.0)
+    reload_time = models.FloatField(null=False, default=0.0)
+    max_ammo = models.FloatField(null=False, default=0.0)
+    damage_falloff_percent = models.FloatField(null=False, default=0.0)
+    punch_through = models.FloatField(null=False, default=0.0)
 
+    impact = models.FloatField(null=False, default=0.0)
+    puncture = models.FloatField(null=False, default=0.0)
+    slash = models.FloatField(null=False, default=0.0)
 
-class WeaponAttackDamageDisposition(models.Model):
-    weapon_name = models.ForeignKey(WeaponAttackStats, on_delete=models.CASCADE)
-    impact = models.FloatField()
-    puncture = models.FloatField()
-    slash = models.FloatField()
+    heat = models.FloatField(null=False, default=0.0)
+    cold = models.FloatField(null=False, default=0.0)
+    electric = models.FloatField(null=False, default=0.0)
+    toxin = models.FloatField(null=False, default=0.0)
+
+    blast = models.FloatField(null=False, default=0.0)
+    corrosive = models.FloatField(null=False, default=0.0)
+    gas = models.FloatField(null=False, default=0.0)
+    magnetic = models.FloatField(null=False, default=0.0)
+    radiation = models.FloatField(null=False, default=0.0)
+    viral = models.FloatField(null=False, default=0.0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['weapon_name', 'attack_type'], name='unique_attack')
+        ]
 
     def __str__(self):
         return self.weapon_name.__str__()

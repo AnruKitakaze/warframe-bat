5dd6a81feabad8b45385ffd41feb48904d98a3d3
AnruKitakaze <groxattack@gmail.com>
2021-05-24 01:18:11 +0700

    feat: Added Target selection and Target level range into home page

--- warframe_bat_project/bat/views.py
+++ warframe_bat_project/bat/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Weapon
+from .models import Weapon, Target
 
 
 def home(request):
     context = {
         'weapons': Weapon.objects.all(),
+        'targets': Target.objects.all(),
         'title': 'Home'
     }
     return render(request, 'bat/home.html', context)
